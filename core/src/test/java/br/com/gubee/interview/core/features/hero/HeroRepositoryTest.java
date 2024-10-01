72bc0a516cd1ff750c22aee264413617753889ef
Guilherme Hermes <85593021+GuilhermeHermes@users.noreply.github.com>
2024-10-01 15:19:38 -0300

    Update HeroRepositoryTest.java

--- core/src/test/java/br/com/gubee/interview/core/features/hero/HeroRepositoryTest.java
+++ core/src/test/java/br/com/gubee/interview/core/features/hero/HeroRepositoryTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -30,9 +31,8 @@ class HeroRepositoryTest {
 
     @DynamicPropertySource
     static void containersProperties(DynamicPropertyRegistry registry) {
-        mongoDBContainer.start();
-        registry.add("spring.data.mongodb.host", mongoDBContainer::getHost);
-        registry.add("spring.data.mongodb.port", () -> mongoDBContainer.getMappedPort(27017));;
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+                                                                                                                          
     }
 
     @BeforeEach
